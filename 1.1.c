d42f42a2cc9d9794551f173ee0e5cbd48b95c499
Vv3n <kittinanwutthi@gmail.com>
2025-12-02 19:14:10 +0700

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int Enternum;
+    printf("Please Enter any number: ");
+
+    if(scanf("%d", &Enternum) != 1){
+        printf("This is not a number goodby!");
+        return 1;
+    }
+    
+    for(int i = 1; i <= Enternum; i++){
+        printf("[%d] Hello world\n", i);
+    }
+
+    return 0;
+}
