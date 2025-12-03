039cad8f9cfe8b13bb4163e88c872e09d8f983e9
Vv3n <kittinanwutthi@gmail.com>
2025-12-03 13:27:25 +0700

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
 void GetSet( int data[], int *num );
-
 int main(){
     int data[100];
     int num;
     GetSet(data, &num);
     return 0;
 }
-
 void GetSet(int data[],int *num){
     printf("Enter number of elements: ");
         if(scanf("%d", num) != 1){
             printf("No number goodbye!");
             return 0;
     }
-
     for(int i = 0; i < *num; i++){
         printf("Enter Number %d: ", i+1);
         if(scanf("%d",&data[i]) != 1){
             printf("No number goodbye!");
         }
     }
-
     printf("\nYour number is:\n");
     for(int i = 0; i < *num; i++){
         printf("%d",data[i]);
