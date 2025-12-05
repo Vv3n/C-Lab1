126fd9fa02ae5e97e734054d05e080f071c2e1d9
Vv3n <kittinanwutthi@gmail.com>
2025-12-05 23:11:10 +0700

    Add files via upload

---
+++ 1.3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *GetSet( int *num);
+
+int main(){
+    int *data, num;
+    data = GetSet(&num);
+
+    for(int i = 0; i < num; i++){
+        printf("%d ", data[i]);
+    }
+    return 0;
+}
+
+int *GetSet( int *num){
+    static int arr[100];
+    printf("Enter number of elements: ");\
+    if(scanf("%d", num) != 1){
+        printf("No number Goodby!");
+        return 0;
+    }
+    for(int i = 0; i < *num; i++){
+        printf("Enter number %d: ",i+1);
+        scanf("%d", &arr[i]);
+    }
+    return arr;
+}
